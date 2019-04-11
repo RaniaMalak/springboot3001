22e46876384693703894945e49ddd369f6e9c758
GBTC440005UR <rania.malak2018fd@gmail.com>
2019-04-10 20:14:37 -0400

    Initial commit

---
+++ src/test/java/com/rm/springboot3001/Springboot3001ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rm.springboot3001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot3001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
